edfd7575795ada3821fbfbdcd432a7b3015bcba7
DanFilipe2000 <anielilipe90@gmail.com>
2023-01-16 23:04:12 -0300

    add comment

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class DocumentController extends Controller
 {
     public function store(Request $request) {
+        // Metodo para capturar nome do arquivo
+        // https://www.php.net/manual/pt_BR/features.file-upload.post-method.php
+        
         $name_document = $_FILES['file']['name'];
 
         $request->file->storeAs('/public/documents', $name_document);
